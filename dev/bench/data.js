89644a5c3ab7db41f5ab6e49d19da42fe2ba66b9
github-action-benchmark <github@users.noreply.github.com>
2024-11-27 06:00:12 +0000

    add benchmark (customSmallerIsBetter) benchmark result for 7c1905687f17695494788ae7891242aa41889af6

---
+++ dev/bench/data.js
@@ -0,0 +1,48 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1732687212121,
+  "repoUrl": "https://github.com/Abingcbc/ilogtail",
+  "entries": {
+    "benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "Abingcbc",
+            "username": "Abingcbc"
+          },
+          "committer": {
+            "name": "Abingcbc",
+            "username": "Abingcbc"
+          },
+          "id": "7c1905687f17695494788ae7891242aa41889af6",
+          "message": "fix",
+          "timestamp": "2024-11-27T03:52:31Z",
+          "url": "https://github.com/Abingcbc/ilogtail/pull/9/commits/7c1905687f17695494788ae7891242aa41889af6"
+        },
+        "date": 1732687211215,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CPU_Usage_Max - performance_file_to_blackhole_ilogtail",
+            "value": 46.06107323932308,
+            "unit": "%"
+          },
+          {
+            "name": "CPU_Usage_Avg - performance_file_to_blackhole_ilogtail",
+            "value": 37.71921665558368,
+            "unit": "%"
+          },
+          {
+            "name": "Memory_Usage_Max - performance_file_to_blackhole_ilogtail",
+            "value": 48.0859375,
+            "unit": "MB"
+          },
+          {
+            "name": "Memory_Usage_Avg - performance_file_to_blackhole_ilogtail",
+            "value": 43.4112980769231,
+            "unit": "MB"
+          }
+        ]
+      }
+    ]
+  }
+}
